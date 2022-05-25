26426c70365e8784c062fe42661072331f40b0fe
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-05-25 14:02:55 -0400

    commit tree 246f73652d57f01a59dce4e887c77f82fcc93f70

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+  belongs_to :actor
+
+  def say_that_thing_you_say
+    "#{self.first_name} #{self.last_name} always says: #{self.catchphrase}"
+  end  
 end
